84f4f8ef2e9436950d37d01edaaddb56881582b9
bulbul07biswas <bulbul07biswas@gmail.com>
2024-07-08 16:03:39 +0530

    dataType

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId= 123
+let accountName="Bulbul"
+var accountMail="bulbulbiswa07@gmail.com"
+accountCity = "Indore"
+let accountState
+
+
+// accountId=10
+accountName="Renne"
+accountMail="Bulbul@yahoo.com"
+accountCity="Raipur"
+
+console.table([accountId,accountName,accountMail,accountCity,accountState]);
+
+
+/*
+Do not use "Var", Because 
+the issue in blockscope and functional scope */
--- Text.js
+++
@@ -1,5 +0,0 @@
-console.log("Java Script Beginners");
-a=10
-b=25
-c=a+b;
-console.log(c)
