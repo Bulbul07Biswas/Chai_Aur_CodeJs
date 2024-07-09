4deaf9b1bf3d5d9e2686eb6b6159cc48625d0838
bulbul07biswas <bulbul07biswas@gmail.com>
2024-07-09 18:49:23 +0530

    Data Type Conversion

---
+++ 01_Basics/03_dataTypeConversion.js
@@ -0,0 +1,55 @@
+/*let score=null
+console.log(score);
+
+let IsNumber= Number(score)
+console.log(typeof IsNumber);
+console.log(IsNumber);*/
+
+/*let score=undefined
+console.log(score);
+
+let IsNumber= Number(score)
+console.log(typeof IsNumber);
+console.log(IsNumber)*/
+
+/*let score= ""
+console.log(score);
+
+let IsNumber= Number(score)
+console.log(typeof IsNumber);
+console.log(IsNumber)*/
+
+/*let score = false
+console.log(score)
+
+let IsNumber= Number(score)
+console.log(typeof score);
+console.log(IsNumber);*/
+
+/*let score = 1
+let IsNumber = Boolean(score)
+console.log(typeof IsNumber);
+console.log(IsNumber)*/
+
+
+/*let value= "Bulbul"
+let IsNumber = Number(value)
+console.log(typeof IsNumber);
+console.log(IsNumber);*/
+
+/*let IsloggeedIn ="Bulbul"
+let checkedLogIn= Boolean(IsloggeedIn)
+console.log(checkedLogIn)
+console.log(typeof checkedLogIn)*/
+
+let num= 45
+let change= String(num)
+console.log(change);
+console.log(typeof change);
+
+/* When we convert this
+"33"->33
+"33abc"->Nan
+True->1; False->0 *
+"Bulbul"->True/
+
