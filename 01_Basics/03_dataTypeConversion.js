f952fbdd60141bcf6579d5a48e82c3050e51216f
bulbul07biswas <bulbul07biswas@gmail.com>
2024-07-09 18:59:46 +0530

    Data Type Conversion

--- 01_Basics/03_dataTypeConversion.js
+++ 01_Basics/03_dataTypeConversion.js
@@ -1,46 +1,46 @@
-/*let score=null
+let score=null
 console.log(score);
 
 let IsNumber= Number(score)
 console.log(typeof IsNumber);
-console.log(IsNumber);*/
+console.log(IsNumber)
 
-/*let score=undefined
-console.log(score);
+let score1=undefined
+console.log(score1);
 
-let IsNumber= Number(score)
-console.log(typeof IsNumber);
-console.log(IsNumber)*/
+let IsNumber1= Number(score1)
+console.log(typeof IsNumber1)
+console.log(IsNumber1)
 
-/*let score= ""
-console.log(score);
+let score2= ""
+console.log(score2);
 
-let IsNumber= Number(score)
-console.log(typeof IsNumber);
-console.log(IsNumber)*/
+let IsNumber2= Number(score2)
+console.log(typeof IsNumber2);
+console.log(IsNumber2)
 
-/*let score = false
-console.log(score)
+let score3 = false
+console.log(score3)
 
-let IsNumber= Number(score)
-console.log(typeof score);
-console.log(IsNumber);*/
+let IsNumber3= Number(score3)
+console.log(typeof score3)
+console.log(IsNumber3)
 
-/*let score = 1
-let IsNumber = Boolean(score)
-console.log(typeof IsNumber);
-console.log(IsNumber)*/
+let score4 = 1
+let IsNumber4 = Boolean(score4)
+console.log(typeof IsNumber4)
+console.log(IsNumber4)
 
 
-/*let value= "Bulbul"
-let IsNumber = Number(value)
-console.log(typeof IsNumber);
-console.log(IsNumber);*/
+let value5= "Bulbul"
+let IsNumber5 = Number(value5)
+console.log(typeof IsNumber5);
+console.log(IsNumber5)
 
-/*let IsloggeedIn ="Bulbul"
+let IsloggeedIn ="Bulbul"
 let checkedLogIn= Boolean(IsloggeedIn)
 console.log(checkedLogIn)
-console.log(typeof checkedLogIn)*/
+console.log(typeof checkedLogIn)
 
 let num= 45
 let change= String(num)
@@ -50,6 +50,9 @@ console.log(typeof change);
 /* When we convert this
 "33"->33
 "33abc"->Nan
-True->1; False->0 *
-"Bulbul"->True/
+True->1; False->0 
+"Bulbul"->True*/
+
+
+//****************Operations***********//
 
