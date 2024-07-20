084c7087d1631d7172a3844724a9e41399344d2b
bulbul07biswas <bulbul07biswas@gmail.com>
2024-07-20 17:07:01 +0530

    dataType

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use Strict"  //Treat all Java code as newer version 
+//alert(3+3) // we are using node js, not browser
+//console.log(3+3);
+
+
+ let name="Shivgami"
+ let age=32
+ let mailId="Shiva@yahoo.com"
+ let loggedIn= true
+ console.table{[typeof "name",typeof age,typeof ]}
+
+
+
+
+// Data Types//
+
+/*
+   Premetive Data type
+Numbers= 2 to the power 53
+Bigint= for large numbers
+String="Bulbul"
+boolean=true/false
+Null= Stand Alone value(Null is representaton of empty value)
+Undefind= 
+symbol=unique
+*/
